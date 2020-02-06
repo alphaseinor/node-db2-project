c991424b1e57bd3bb15a4a1a6778e433c7495f49
Brian Hague <alphaseinor@gmail.com>
2020-02-06 17:46:56 -0600

    consolidated cars and car_visit migrations

---
+++ migrations/20200204205753_car_table.js
@@ -0,0 +1,47 @@
+
+exports.up = function(knex) {
+  return ( knex.schema
+    //EndPoint
+    .createTable('cars', (tbl) => {
+      tbl.increments()
+      tbl.timestamps()
+      tbl
+        .string("vin", 17)
+        .notNullable()
+        .unique()
+      tbl
+        .string("year")
+        .notNullable()
+      tbl
+        .string("make")
+        .notNullable()
+      tbl
+        .string("model")
+        .notNullable()
+      tbl
+        .float('initial_mileage')
+        .notNullable()
+    })
+    //has foreign for car_table
+    .createTable('car_visit', (tbl) => {
+      tbl.increments()
+      tbl.timestamps()
+      tbl.string("title_status")
+      tbl.string("miles")
+      tbl.string("engine")
+      tbl.string("transmission")
+      tbl
+        .integer('cars_id')
+      tbl
+        .foreign('id')
+        .references('cars_id')
+        .inTable('cars')
+    })
+};
+
+exports.down = function(knex) {
+  return ( knex.schema
+    .dropTableIfExists('car_visit')
+    .dropTableIfExists('cars')
+  )
+};
